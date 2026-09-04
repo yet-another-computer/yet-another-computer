2ab2cd12d5898053cd6ba0fd866a42f2982d5629
Ivan Tolmachev <ivatolm@gmail.com>
2026-09-04 23:49:45 +0400

    Add left half of the computer

--- main.v
+++ main.v
@@ -8,7 +8,8 @@ module alu #(
     input  wire op_plus,
     input  wire op_and
 );
-    assign out = op_plus ? (a + b) : (op_and ? a & b : 'z);
+    // NOTE: OP_AND IS USED FOR DEBUG PURPOSES FOR NOW
+    assign out = op_plus ? (a + b) : (op_and ? a : 'z);
 endmodule
 
 module ram #(
@@ -49,6 +50,17 @@ module controller #(
     output wire _gate_ip_oe,
 
     output wire _gate__bus__reg_ram_value_in__signal,
+    
+    output wire _gate_a_we,
+    output wire _gate_a_oe,
+
+    output wire _gate_b_we,
+    output wire _gate_b_oe,
+
+    output wire _gate_alu_op_plus,
+    output wire _gate_alu_op_and,
+
+    output wire _gate__alu_out__bus__signal,
 
     input wire clock,
     input wire reset
@@ -56,31 +68,43 @@ module controller #(
     // NOTE: For debug purposes
     // reg [3:0] step;
     always @(posedge clock or posedge reset) begin
-        if (reset)
+        if (reset || _gate_cmd_we)
             step <= 0;
         else
             step <= step + 1;
     end
 
+    localparam RAM_ADDRESS_WE                = 32'b0000000000000000001;
+    localparam RAM_VALUE_WE                  = 32'b0000000000000000010;
+    localparam IP_WE                         = 32'b0000000000000000100;
+    localparam CMD_WE                        = 32'b0000000000000001000;
+    localparam RAM_ADDRESS_OE                = 32'b0000000000000010000;
+    localparam RAM_VALUE_OE                  = 32'b0000000000000100000;
+    localparam IP_OE                         = 32'b0000000000001000000;
+    localparam BUS__REG_RAM_VALUE_IN__SIGNAL = 32'b0000000000010000000;
+    localparam A_WE                          = 32'b0000000000100000000;
+    localparam A_OE                          = 32'b0000000001000000000;
+    localparam B_WE                          = 32'b0000000010000000000;
+    localparam B_OE                          = 32'b0000000100000000000;
+    localparam ALU_OP_PLUS                   = 32'b0000001000000000000;
+    localparam ALU_OP_AND                    = 32'b0000010000000000000;
+    localparam ALU_OUT__BUS__SIGNAL          = 32'b0000100000000000000;
 
-    localparam RAM_ADDRESS_WE                = 8'b00000001;
-    localparam RAM_VALUE_WE                  = 8'b00000010;
-    localparam IP_WE                         = 8'b00000100;
-    localparam CMD_WE                        = 8'b00001000;
-    localparam RAM_ADDRESS_OE                = 8'b00010000;
-    localparam RAM_VALUE_OE                  = 8'b00100000;
-    localparam IP_OE                         = 8'b01000000;
-    localparam BUS__REG_RAM_VALUE_IN__SIGNAL = 8'b10000000;
-
-    reg [8-1:0] gate_table[2**WIDTH];
+    localparam INSTRUCTION_COUNT = 16;
+    
+    reg [32-1:0] gate_table[2**WIDTH];
     initial begin
         integer i;
         for (i = 0; i < 2**WIDTH; i = i + 1)
-            gate_table[i] = 8'b0;
+            gate_table[i] = 32'b0;
 
-        gate_table[0] = IP_OE | RAM_ADDRESS_WE;
-        gate_table[1] = RAM_ADDRESS_OE | RAM_VALUE_WE;
-        gate_table[2] = RAM_VALUE_OE | CMD_WE;
+        gate_table[4'b0000 * INSTRUCTION_COUNT + 0] = IP_OE | RAM_ADDRESS_WE;
+        gate_table[4'b0000 * INSTRUCTION_COUNT + 1] = RAM_ADDRESS_OE | RAM_VALUE_WE;
+        gate_table[4'b0000 * INSTRUCTION_COUNT + 2] = RAM_VALUE_OE | CMD_WE;
+
+        gate_table[4'b0001 * INSTRUCTION_COUNT + 0] = A_OE | ALU_OP_AND | ALU_OUT__BUS__SIGNAL | RAM_ADDRESS_WE;
+        gate_table[4'b0001 * INSTRUCTION_COUNT + 1] = RAM_ADDRESS_OE | RAM_VALUE_WE;
+        gate_table[4'b0001 * INSTRUCTION_COUNT + 2] = RAM_VALUE_OE | A_WE;
     end
 
     wire [7:0] pointer;
@@ -94,6 +118,13 @@ module controller #(
     assign _gate_ram_value_oe                   = gate_table[pointer][5];
     assign _gate_ip_oe                          = gate_table[pointer][6];
     assign _gate__bus__reg_ram_value_in__signal = gate_table[pointer][7];
+    assign _gate_a_we                           = gate_table[pointer][8];
+    assign _gate_a_oe                           = gate_table[pointer][9];
+    assign _gate_b_we                           = gate_table[pointer][10];
+    assign _gate_b_oe                           = gate_table[pointer][11];
+    assign _gate_alu_op_plus                    = gate_table[pointer][12];
+    assign _gate_alu_op_and                     = gate_table[pointer][13];
+    assign _gate__alu_out__bus__signal          = gate_table[pointer][14];
 endmodule
 
 module computer;
@@ -163,6 +194,43 @@ module computer;
         reset
     );
 
+    wire _gate_a_we;
+    wire _gate_a_oe;
+    wire [15:0] _reg_a_out;
+    register reg_a(
+        bus,
+        _reg_a_out,
+        _gate_a_we,
+        _gate_a_oe,
+        clock,
+        reset
+    );
+
+    wire _gate_b_we;
+    wire _gate_b_oe;
+    wire [15:0] _reg_b_out;
+    register reg_b(
+        bus,
+        _reg_b_out,
+        _gate_b_we,
+        _gate_b_oe,
+        clock,
+        reset
+    );
+
+    wire [15:0] _alu_out;
+    wire _gate_alu_op_plus;
+    wire _gate_alu_op_and;
+    alu alu(_reg_a_out, _reg_b_out, _alu_out, _gate_alu_op_plus, _gate_alu_op_and);
+
+    wire _gate__alu_out__bus__signal;
+    wire [15:0] _alu_out_bus;
+    gate _gate__alu_out_bus(
+        _alu_out,
+        _alu_out_bus,
+        _gate__alu_out__bus__signal
+    );
+    
     wire [3:0] step;
     controller controller(
         _reg_cmd_out,
@@ -179,6 +247,17 @@ module computer;
 
         _gate__bus__reg_ram_value_in__signal,
 
+        _gate_a_we,
+        _gate_a_oe,
+
+        _gate_b_we,
+        _gate_b_oe,
+
+        _gate_alu_op_plus,
+        _gate_alu_op_and,
+
+        _gate__alu_out__bus__signal,
+
         clock,
         reset
     );
@@ -187,11 +266,13 @@ module computer;
                 _gate_ip_oe ?
                     _reg_ip_out :
                 _gate_ram_value_oe ?
-                    {8'bx, _reg_ram_value_out} :
+                    {8'b0, _reg_ram_value_out} :
+                _gate__alu_out__bus__signal ?
+                    _alu_out_bus :
                 16'bx;
 
    	initial begin
-        $monitor("TICK=%0t STEP=%0d RESET=%b BUS=%b RAM_ADDR=%b RAM_VALUE=%b IP=%b CMD=%b",
+        $monitor("TICK=%0t STEP=%0d RESET=%b BUS=%h RAM_ADDR=%h RAM_VALUE=%h IP=%h CMD=%h A=%h B=%h",
             $time,
             step,
             reset,
@@ -199,7 +280,9 @@ module computer;
             _reg_ram_address_out,
             _reg_ram_value_out,
             _reg_ip_out,
-            _reg_cmd_out
+            _reg_cmd_out,
+            _reg_a_out,
+            _reg_b_out
         );
 
         reset <= 1;
